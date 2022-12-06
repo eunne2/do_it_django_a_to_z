86535c2b918e5cd4904876120afff8f344fd2e5c
EunHye LEE <ehye518@gmail.com>
2022-12-06 15:50:41 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -71,6 +71,12 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return '../../static/profile.png/'
+
     def __str__(self):
         return f'{self.author}::{self.content}'
 
